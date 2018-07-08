5bf79756eae4255491efb20211f70ed8cf4695e8
maddyv26 <madelyne.voreh@salem.edu>
2018-07-07 21:30:43 -0400

    Initial Commit

---
+++ I AM RICH/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  I AM RICH
+//
+//  Created by Madelyne Voreh on 7/6/18.
+//  Copyright © 2018 Madelyne Voreh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
